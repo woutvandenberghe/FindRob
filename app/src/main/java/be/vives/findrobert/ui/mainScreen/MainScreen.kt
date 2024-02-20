2bc9f8ecad719469535f108cdde0c74eee76373a
r0928538 <115366424+r0928538@users.noreply.github.com>
2024-02-20 10:46:43 +0100

    mainscreen fix

--- app/src/main/java/be/vives/findrobert/ui/mainScreen/MainScreen.kt
+++ app/src/main/java/be/vives/findrobert/ui/mainScreen/MainScreen.kt
@@ -10,16 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +25,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun MainScreen(modifier: Modifier, mainScreenViewModel: MainScreenViewModel) {
+fun MainScreen(modifier: Modifier) {
     val mainScreenViewModel: MainScreenViewModel = viewModel(
         factory = MainScreenViewModel.Factory
     )
