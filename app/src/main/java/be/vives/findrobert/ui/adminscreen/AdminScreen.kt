ea079f1c3eed9602981827d7b4d1cbdeb3425783
r0928538 <115366424+r0928538@users.noreply.github.com>
2024-02-20 09:52:37 +0100

    voorlopig adminscreen idk ik ga hem updaten wanneer er database is - hint aangepast om te matchen met main screen

--- app/src/main/java/be/vives/findrobert/ui/adminscreen/AdminScreen.kt
+++ app/src/main/java/be/vives/findrobert/ui/adminscreen/AdminScreen.kt
@@ -26,10 +26,16 @@ fun AdminScreen(modifier: Modifier = Modifier,
                 .padding(20.dp, 80.dp, 20.dp, 20.dp)
         ) {
             Text(stringResource(id = R.string.currentHint))
+            //Mock hint
+            Text("Robert ligt dicht bij de grote boom.")
             //hier hint uit database halen
             Text(stringResource(id = R.string.currentLocation))
+            //Mock locatie
+            Text("Robert ligt bij de eerste bom links van de Radio 2 gebouw aan de kant van de straat.")
             //hier locatie uit database halen
             Text(stringResource(id = R.string.lastFoundBy))
+            //Mock persoon
+            Text("Sven Lysiak")
             //hier persoon uit database halen
         }
         if (!viewmodel.isAdmin()) {
